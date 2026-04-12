5f5ffb4ca5a056749b48518c060b42b90794eff5
Kwadwo Larbi <larbikwadwo.05@gmail.com>
2026-04-11 23:11:34 -0400

    Creating an internal database with list

---
+++ utils/database.py
@@ -0,0 +1,31 @@
+"""
+Concerned with retrieving  and  storing books from a  list.
+Concerned with retrieving  and  storing books from a  csv file
+format of the csv file:
+name#author#read
+"""
+books = []
+
+
+def add_book(name, author):
+    books.append({"name": name, "author": author, 'read':False})
+
+
+def get_all_books():
+    return books
+
+
+def mark_book_read(name):
+    for book in books:
+        if book["name"] == name:
+            book["read"] = True
+
+
+def delete_book(name):
+    global books
+    books = [book for book in books if book['name'] != name] # add each book to a new list if book['name'] != name
+
+#def delete_book(name):
+ #   for book in books:
+     #   if book["name"] == name:
+#            books.remove(book)

5b711e5a1614a8c5e4bd08d4eb91f8737b8e2b5d
Kwadwo Larbi <larbikwadwo.05@gmail.com>
2026-08-25 19:55:41 -0400

    Add files via upload

---
+++ Python fresh/name_case.py
@@ -0,0 +1,17 @@
+name = "Kwadwo"
+print("Hello", name + ",","would you like to learn some Python today ?" )
+
+name2 = "Kenny Osten Larbi Kwadwo"
+print(name2.lower())
+print(name2.upper())
+print(name2.title())
+
+famous_person = 'Albert Einstein once said,"A person who never made a mistake never tried anything new."'
+message = famous_person
+print(message)
+
+name3 = " \tKwadwo Larbi \n"
+print(name3)
+print(name3.rstrip())
+print(name3.lstrip())
+print(name3.strip())
